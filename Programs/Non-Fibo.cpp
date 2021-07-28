2ddae41c9ed9608a376748d3402b3d1c308bc358
Crazycoder122 <prodoshcmitterfeluda@gmail.com>
2021-07-28 18:00:49 +0530

    Commented the Code

--- Programs/Non-Fibo.cpp
+++ Programs/Non-Fibo.cpp
@@ -6,14 +6,18 @@ vector<int>ans;
 
 void non_fibo(int current_fibo,int next_fibo,int current_non_fibo,int target_size)
 {
+
+    // Boundary Condition for the Recursion to stop when the list_size has been fulfilled
     if(target_size==ans.size())
     return;
 
+    // If current_non_fibo becomes equal to the next fibo, then we call the function by replacing the current_fibo with next_fibo and next_fibo is replaced by (current_fibo+next_fibo)
     if(next_fibo==current_non_fibo)
     {
         non_fibo(next_fibo,current_fibo+next_fibo,next_fibo+1,target_size);
     }
 
+    // The Required Non-Fibo Number is pushed to the answer list and then function is called with [(current_non_fibo)+1] in place of current_non_fibo
     else
     {
         ans.push_back(current_non_fibo);
--- Programs/remove_comments.c
+++ Programs/remove_comments.c
@@ -5,6 +5,8 @@
 int avoid_count_single_line_comment(char *a,int start)
 {
     int i,ans=0;
+
+    // Checking until '\n' is encountered
     for(i=start;;i++)
     {
         if(a[i]=='\n')
@@ -18,6 +20,8 @@ int avoid_count_single_line_comment(char *a,int start)
 int avoid_count_multi_line_comment(char *a,int start)
 {
     int i,ans=0;
+
+    // Checking until '*/' is encountered
     for(i=start;;i++)
     {
         if(a[i]=='*' && a[i+1]=='/')
@@ -33,20 +37,23 @@ char * remove_comments(char *code)
 {
     int len=strlen(code),index=0,i;
     static char ans[100000];
-    bool multi_line=false,single_line=false;
 
-    for(i=0;i<len;i++)
+    // Traversing the string
+    for(i=0;i<len;i++) 
     {
+        // If single_line_comment is encountered, then we get the skip_value from the avoid_count_single_line_comment() function and 'i' is incremented by the correpsonding skip_value(Adjusted with some measures)
         if(code[i]=='/' && code[i+1]=='/')
         {
             i+=avoid_count_single_line_comment(code,i+2)+1;
         }
 
+        // If multi_line_comment is encountered, then we get the skip_value from the avoid_count_multi_line_comment() function and 'i' is incremented by the correpsonding skip_value(Adjusted with some measures)
         else if(code[i]=='/' && code[i+1]=='*')
         {
             i+=avoid_count_multi_line_comment(code,i+2)+3;
         }
 
+        // The Character is inserted to the 'ans' string
         else
             ans[index++]=code[i];
     }
